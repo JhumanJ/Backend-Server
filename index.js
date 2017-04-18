1634540d845e26492b9bf7c7539d89fa61a559b8
JhumanJ <julien@nahum.net>
2017-04-18 15:03:09 +0100

    Add body-parser to app

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 
-require('./api/routes')(app);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
-   if (req.method === 'OPTIONS') {
-    res.statusCode = 204;
-    return res.end();
-  } else {
-    return next();
-  }
-});
+require('./api/routes')(app);
 
 app.listen(8080, function () {
   console.log('API listening on port 8080!');
